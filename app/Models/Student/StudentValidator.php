c2848523d7af325ab1e1a11d682ef8b10263b6a7
Gleuton Dutra <gleuton.dutra@gmail.com>
2025-04-22 20:42:54 -0300

    fix: ajusta validador do cpf

--- app/Models/Student/StudentValidator.php
+++ app/Models/Student/StudentValidator.php
@@ -106,7 +106,7 @@ class StudentValidator
             $sum += (int)$cpf[$i] * $weight--;
         }
         $checkDigit = ($sum % 11) < 2 ? 0 : 11 - ($sum % 11);
-        if ($checkDigit !== $cpf[9]) {
+        if ($checkDigit != $cpf[9]) {
             return false;
         }
 
@@ -114,7 +114,7 @@ class StudentValidator
             $sum += (int)$cpf[$i] * $weight--;
         }
         $checkDigit = ($sum % 11) < 2 ? 0 : 11 - ($sum % 11);
-        return $checkDigit === $cpf[10];
+        return $checkDigit == $cpf[10];
     }
 
     private function validatePassword(string $password): bool
