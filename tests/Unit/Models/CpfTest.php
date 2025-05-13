317fab102316b6ccd51db1077f2e0043afd9d17c
Gleuton Dutra <gleuton.dutra@gmail.com>
2025-05-13 20:30:37 -0300

    test: teste unitario para o CPF

---
+++ tests/Unit/Models/CpfTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Models;
+
+use FiapAdmin\Exceptions\ValidationException;
+use FiapAdmin\Models\Cpf;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class CpfTest extends TestCase
+{
+    #[DataProvider('validCpfProvider')]
+    public function testValidCpfCreatesInstance(string $input): void
+    {
+        $cpf = new Cpf($input);
+        $this->assertSame($input, $cpf->value());
+    }
+
+    public static function validCpfProvider(): array
+    {
+        return [
+            'plain digits' => ['11144477735'],
+            'formatted with dots and dash' => ['111.444.777-35'],
+        ];
+    }
+
+    #[DataProvider('invalidCpfProvider')]
+    public function testInvalidCpfThrowsException(string $input): void
+    {
+        $this->expectException(ValidationException::class);
+        new Cpf($input);
+    }
+
+    public static function invalidCpfProvider(): array
+    {
+        return [
+            'too short' => ['123'],
+            'too long' => ['123456789012'],
+            'repeated digits' => ['11111111111'],
+            'invalid check digits' => ['11144477734'],
+            'formatted invalid' => ['111.444.777-34'],
+        ];
+    }
+}
